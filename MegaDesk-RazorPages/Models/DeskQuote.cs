a4884c23eefefccae9eb9084239212417c454df7
Troy Davidson <troymd2009@hotmail.com>
2022-11-05 16:30:34 -0600

    commit classes

---
+++ MegaDesk-RazorPages/Models/DeskQuote.cs
@@ -0,0 +1,155 @@
+﻿namespace MegaDesk_RazorPages.Models
+{
+    public class DeskQuote
+    {
+        public string CustomerName { get; set; }
+        public string QuoteDate;
+        public float RushDays { get; set; }
+
+        public Desk newDesk = new Desk();
+
+        public float DrawerCost { get; set; }
+
+        public float QuotePrice { get; set; }
+        public float RushCost { get; set; }
+
+
+
+        public float MaterialCost;
+        public float SurfaceArea;
+        public float SizeCost;
+
+        private const int BASE_PRICE = 200;
+        private const int SIZE_TRESHHOLD = 1000;
+        private const int PRICE_PER_DRAWER = 50;
+
+
+
+        public float CalcMaterialCost(string material)
+        {
+            newDesk.DeskMaterial = material;
+
+            switch (newDesk.DeskMaterial)
+            {
+                case "Oak":
+                    MaterialCost = 200;
+                    break;
+
+                case "Laminate":
+                    MaterialCost = 100;
+                    break;
+
+                case "Pine":
+                    MaterialCost = 50;
+                    break;
+
+                case "Rosewood":
+                    MaterialCost = 300;
+                    break;
+
+                case "Veneer":
+                    MaterialCost = 125;
+                    break;
+            }
+            return MaterialCost;
+        }
+
+
+        public float CalcSurfaceArea(float width, float depth)
+        {
+            newDesk.DeskWidth = width;
+            newDesk.DeskDepth = depth;
+            SurfaceArea = newDesk.DeskWidth * newDesk.DeskDepth;
+            return SurfaceArea;
+        }
+
+
+
+
+        public float CalcSurfaceAreaCost(float SurfaceArea)
+        {
+            if (SurfaceArea <= 1000)
+            {
+                SizeCost = BASE_PRICE;
+            }
+            else
+            {
+                SizeCost = BASE_PRICE + (SurfaceArea - SIZE_TRESHHOLD);
+            }
+
+            return SizeCost;
+        }
+
+
+        public float CalcRushOrderCost(float RushDays, float SurfaceArea)
+        {
+            if (SurfaceArea<1000)
+            {
+                switch (RushDays)
+                {
+                    case 3:
+                        RushCost = 60;
+                        break;
+                    case 5:
+                        RushCost = 40;
+                        break;
+                    case 7:
+                        RushCost = 30;
+                        break;
+                }
+            }
+            else if (SurfaceArea < 2000)
+            {
+                switch (RushDays)
+                {
+                    case 3:
+                        RushCost = 70;
+                        break;
+                    case 5:
+                        RushCost = 50;
+                        break;
+                    case 7:
+                        RushCost = 35;
+                        break;
+                }
+            }
+            else if (SurfaceArea > 2000)
+            {
+                switch (RushDays)
+                {
+                    case 3:
+                        RushCost = 80;
+                        break;
+                    case 5:
+                        RushCost = 60;
+                        break;
+                    case 7:
+                        RushCost = 40;
+                        break;
+                }
+            }
+            return RushCost;
+        }
+        public float CalcDrawerCost()
+        {
+            DrawerCost = newDesk.NumDrawers * PRICE_PER_DRAWER;
+            return DrawerCost;
+        }
+
+        public float CalcTotalCost(float MaterialCost, float DrawerCost, float RushCost)
+        {
+            QuotePrice = SizeCost + MaterialCost + DrawerCost + RushCost;
+            return QuotePrice;
+        }
+
+
+        public DeskQuote()
+        {
+
+            DrawerCost = newDesk.NumDrawers * PRICE_PER_DRAWER;
+
+            QuotePrice = BASE_PRICE + SizeCost + DrawerCost + MaterialCost + RushCost;
+
+        }
+    }
+}
